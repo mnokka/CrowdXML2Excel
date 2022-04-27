65a02a1e63aede1592cbedb37d099110ce061171
Mika Nokka <mika.nokka@digital14.com>
2022-04-27 15:46:05 +0300

    sub dict sizes and data fetched ok

--- converter.py
+++ converter.py
@@ -1,3 +1,8 @@
+#
+# TODO DESC
+#
+# 26.4.2022 mika.nokka1@gmail.com
+
 # original xml to json example: https://www.geeksforgeeks.org/python-xml-to-json/?ref=lbp
 
 
@@ -61,15 +66,67 @@ print("-----------------------------------------------------")
 #    print ("------------------------------------------------------------------------")
 
 
+# len works 
 len=(len(data_dict['crowd']['users']['user']))
 
 for i in range(0,len):
     print ("NUMBER:{0}".format(i))
-    print (data_dict['crowd']['users']['user'][i])
+    #print (data_dict['crowd']['users']['user'][i])
     print (data_dict['crowd']['users']['user'][i]['name'])
+    print (data_dict['crowd']['users']['user'][i]['firstName'])
+    print (data_dict['crowd']['users']['user'][i]['lastName'])
+    print (data_dict['crowd']['users']['user'][i]['email'])
+    print (data_dict['crowd']['users']['user'][i]['credential'])
     print (".....................................................................")
     
-    
-    
-    
+
+#print(data_dict['crowd']['memberships'].items())
+
+#len2=(len(data_dict['crowd']['memberships']['membership']))
+#print (len2)
+
+#print (data_dict['crowd']['memberships']['membership'][1]['parentName'])
+#print (data_dict['crowd']['memberships']['membership'][600]['parentName'])
+
+#print (data_dict['crowd']['memberships'].values())
+
+#print (sum(data_dict['crowd']['memberships'].values()))
+
+
+
+#x=data_dict["crowd"]["memberships"]['membership']
+#len=len(x)
+#print (len)
+
+#print ('......................................................')
+#count=0
+#for key,value in data_dict['crowd']['memberships'].items(): # ajaa kerran
+#    #if isinstance(value,list):
+#    count=count+1
+#    print ("key: {0}  value: {1}".format(key,value))
+#    print ('++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+#print(count)
+
+        
+#for i in range(0,667):
+#    print ("NUMBER:{0}".format(i))
+#    print (data_dict['crowd']['memberships']['membership'][i]['parentName'])
+#    print (data_dict['crowd']['memberships']['membership'][i]['childName'])    
+#    print (".................")
+
+counter=0 #len did not work with membership subdictionary
+for i in data_dict['crowd']['memberships']['membership']:
+    #print (i)
+    counter = counter+1
+    #print ("----------------------------")
+print (counter)    
+
+
+for i in range(0,counter-1):
+    print ("NUMBER:{0}".format(i))
+    print (data_dict['crowd']['memberships']['membership'][i]['parentName'])
+    print (data_dict['crowd']['memberships']['membership'][i]['childName'])    
+    print (".................")
+
+
 

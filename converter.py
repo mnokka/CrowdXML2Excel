344b313adfa58763257bd20ed8780cfa9e5a2a0f
Mika Nokka <mika.nokka@digital14.com>
2022-04-27 17:29:35 +0300

    Creates excels ok

--- converter.py
+++ converter.py
@@ -1,8 +1,15 @@
 #
 # Atlassian Crowd 3.7.0 backup file xml convertor
-# processses file and creted two excels needed to import data to another Crowd
+# processses file and creates two excels needed to import data to another Crowd
 #
-# 26.4.2022 mika.nokka1@gmail.com
+# This script generates two excels; user and group ones. Save them as "csv" (comma delimeter)
+# Save also "empty" users csv file (just titles exists)
+#
+# In Crowd User import section:
+# Phase 1: Import users and groups csv files (creates users and groups)
+# Phase 2: Import Empty users and (normal) groups files (adds users to their groups)
+#
+# (c) 26.4.2022 mika.nokka1@gmail.com
 
 
 import json
@@ -34,15 +41,25 @@ worksheet1.write(0,4,"Password")
 
 
 len=(len(data_dict['crowd']['users']['user'])) # len works
-
+row=1
+column=0
 for i in range(0,len):
     print ("NUMBER:{0}".format(i))
     #print (data_dict['crowd']['users']['user'][i])
-    print (data_dict['crowd']['users']['user'][i]['name'])
-    print (data_dict['crowd']['users']['user'][i]['firstName'])
-    print (data_dict['crowd']['users']['user'][i]['lastName'])
-    print (data_dict['crowd']['users']['user'][i]['email'])
-    print (data_dict['crowd']['users']['user'][i]['credential'])
+    name=data_dict['crowd']['users']['user'][i]['name']
+    firstName=data_dict['crowd']['users']['user'][i]['firstName']
+    lastName=data_dict['crowd']['users']['user'][i]['lastName']
+    email=data_dict['crowd']['users']['user'][i]['email']
+    credential=data_dict['crowd']['users']['user'][i]['credential']
+    print ("{0} {1} {2} {3} {4}".format(name,firstName,lastName,email,credential))
+    
+    worksheet1.write(row,column,name)
+    worksheet1.write(row,column+1,firstName)
+    worksheet1.write(row,column+2,lastName)
+    worksheet1.write(row,column+3,email)
+    worksheet1.write(row,column+4,credential)
+    row=row+1
+    
     print (".....................................................................")
     
 
@@ -57,11 +74,19 @@ for i in data_dict['crowd']['memberships']['membership']:
     #print (i)
     counter = counter+1
     #print ("----------------------------------------------------------------------")
-    
+
+row=1
+column=0    
 for i in range(0,counter-1):
     print ("NUMBER:{0}".format(i))
-    print (data_dict['crowd']['memberships']['membership'][i]['parentName'])
-    print (data_dict['crowd']['memberships']['membership'][i]['childName'])    
+    parentName=(data_dict['crowd']['memberships']['membership'][i]['parentName'])
+    childName=(data_dict['crowd']['memberships']['membership'][i]['childName'])    
+    print ("{0} {1}".format(parentName,childName))
+
+    worksheet2.write(row,column,childName)
+    worksheet2.write(row,column+1,parentName)
+    row=row+1
+    
     print (".......................................................................")
 
 
